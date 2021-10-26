80191c77b8b4c384b8e89d167bd50ef1c0306983
stephenshears <60107098+stephenshears@users.noreply.github.com>
2021-10-26 00:40:00 -0400

    addPage_Process.php
    Create the page where the movie will be inserted into the database.

---
+++ addPage_Process.php
@@ -0,0 +1,21 @@
+<?php
+
+    require_once('classes/Movie.php');
+
+    $movie = new Movie;
+
+    $movie->settitle($_REQUEST['title']);
+    $movie->setbudget($_REQUEST['budget']);
+    $movie->setreleaseDate($_REQUEST['release_date']);
+    $movie->setruntime($_REQUEST['runtime']);
+    $movie->setDescription($_REQUEST['description']);
+    $movie->setURL($_REQUEST['URL']);
+    $movie->setembargo($_REQUEST['embargo']);
+
+    if ($movie->save()) {
+        header("Location: ./");
+    } else {
+        header("Location: ./?action=addPage&error=1");
+    }
+
+?>
