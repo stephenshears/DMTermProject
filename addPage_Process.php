8f9eb71090ebcebc02f6b868b012e5a301730e77
stephenshears <sws30@zips.uakron.edu>
2021-11-09 15:15:16 -0500

    Fix some issues with directly connecting to the process pages

--- addPage_Process.php
+++ addPage_Process.php
@@ -1,21 +1,27 @@
 <?php
 
-    require_once('classes/Movie.php');
+    if(!empty($_POST)){
+        require_once('classes/Movie.php');
 
-    $movie = new Movie;
+        $movie = new Movie;
 
-    $movie->settitle($_REQUEST['title']);
-    $movie->setbudget($_REQUEST['budget']);
-    $movie->setreleaseDate($_REQUEST['release_date']);
-    $movie->setruntime($_REQUEST['runtime']);
-    $movie->setDescription($_REQUEST['description']);
-    $movie->setURL($_REQUEST['URL']);
-    $movie->setembargo($_REQUEST['embargo']);
+        $movie->settitle($_REQUEST['title']);
+        $movie->setbudget($_REQUEST['budget']);
+        $movie->setreleaseDate($_REQUEST['release_date']);
+        $movie->setruntime($_REQUEST['runtime']);
+        $movie->setDescription($_REQUEST['description']);
+        $movie->setURL($_REQUEST['URL']);
+        $movie->setembargo($_REQUEST['embargo']);
 
-    if (!$movie->save()) {
-        header("Location: ./");
-    } else {
-        header("Location: ./?action=addPage&error=1");
+        if (!$movie->save()) {
+            header("Location: ./");
+        } else {
+            header("Location: ./?action=addPage&error=1");
+        }
+    }
+    else{
+        print("Access Denied");
+        exit();
     }
 
 ?>
